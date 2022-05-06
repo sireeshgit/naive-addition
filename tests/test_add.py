e493d5cd611f62b6cf131e707db28bd4766b4148
sireeshgit <sireesh.dadi@gmail.com>
2022-05-05 20:44:14 -0500

    <code> added with an array

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,4 +1,9 @@
 from src.add import add
+import numpy as np
 
 def test_add():
     assert add(1) == 101
+
+def test2_add():
+    a = add(np.array([1,2,3,4])) == np.array([101,102,103,104])
+    assert a.all()
